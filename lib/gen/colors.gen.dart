6d8aca9f755f584c153c0c55f6329cdcd0cb6786
marwa-my <engineer.marwatalaat@gmail.com>
2024-06-15 06:48:04 +0300

    adding colors and screen util

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,33 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #03DAC5
+  static const Color colorAccent = Color(0xFF03DAC5);
+
+  /// Color: #FF4C3B
+  static const Color colorPrimary = Color(0xFFFF4C3B);
+
+  /// Color: #3700B3
+  static const Color colorPrimaryDark = Color(0xFF3700B3);
+
+  /// Color: #777777
+  static const Color colorText = Color(0xFF777777);
+
+  /// Color: #222222
+  static const Color colorTitle = Color(0xFF222222);
+
+  /// Color: #FFFFFF
+  static const Color colorWhite = Color(0xFFFFFFFF);
+}
